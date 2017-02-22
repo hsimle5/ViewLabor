d1716a051e9b86c996c4962fecf576ad1b2ee782
WKSMISU\soma <hok.soma@flexisys.hu>
2017-02-22 16:22:03 +0100

    Initial commit

---
+++ app/src/main/java/examples/amorg/aut/bme/hu/viewlabor/ViewLaborActivity.java
@@ -0,0 +1,15 @@
+package examples.amorg.aut.bme.hu.viewlabor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLaborActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_labor);
+    }
+
+    //PasswordEditText passwordEditText = (PasswordEditText) findViewById(R.id.registrationPET);
+}
